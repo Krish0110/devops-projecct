b72416fe03e678c27f970aa1b5964cafbba565f3
Krishala Prajapati <077bct038.krishala@pcampus.edu.np>
2025-03-21 13:22:33 +0545

    test change made

--- major-web-frontend/src/components/AboutUsComponent/AboutUsComponent.js
+++ major-web-frontend/src/components/AboutUsComponent/AboutUsComponent.js
@@ -6,11 +6,15 @@ const AboutUsComponent = () => {
   return (
     <ContentContainer>
       <DetailsContainer>
-        <h3>Project Overview</h3>
+        <h3>Project Overview </h3>
         <p>Our project focuses on reviving old, damaged photos using advanced AI techniques. With this tool, users can restore torn, scratched, or faded images, preserving their precious memories for years to come.</p>
       </DetailsContainer>
       <DetailsContainer>
         <h3>Purpose and Inspiration</h3>
+        <p>We were inspired by the timeless value of old photographs and the need to preserve them in the digital age. Our goal is to simplify the process of photo restoration and make it accessible to everyone.</p>
+      </DetailsContainer>
+      <DetailsContainer>
+        <h3>Limitations</h3>
         <p>We were inspired by the timeless value of old photographs and the need to preserve them in the digital age. Our goal is to simplify the process of photo restoration and make it accessible to everyone.</p>
       </DetailsContainer>
       <TeamDetails/>
